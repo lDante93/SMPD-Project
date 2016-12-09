c9868fe3bcb839d34b8fbf5635475743fa943881
lDante93 <niletuv@gmail.com>
2016-12-09 15:43:22 +0100

    Pushing Project

---
+++ Project/src/NNClassifier.java
@@ -0,0 +1,26 @@
+
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Arrays;
+import java.util.stream.IntStream;
+import org.apache.commons.lang3.ArrayUtils;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+
+/**
+ * musi być taka sama liczba próbek w klasach
+ * @author Sokol
+ */
+class NNClassifier extends abstractClassifier {
+
+    NNClassifier(PR_GUI gui) {
+        pr_gui = gui;
+    }    
+    
+} 
